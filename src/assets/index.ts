dfa722fce7981241319f4b253ece77364ec278d4
Stas9364 <stas-lisovskii@mail.ru>
2022-08-29 13:33:23 +0300

    commit

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,7 +6,7 @@ import edit from './img/edit.svg';
 import logo from './img/logo.png';
 import mail from './img/mail.png';
 import oldCar from './img/oldCar.svg';
-import preloader from './img/preloader.gif';
+import preloader from './img/new_preloader.gif';
 import study from './img/school.svg';
 import search from './img/search.svg';
 import pen from './img/pen.svg';
