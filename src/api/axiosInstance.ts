f66658ee313e84db99733eced347afb8b9e5ca70
Stas9364 <stas-lisovskii@mail.ru>
2022-10-31 14:39:28 +0300

    commit

--- src/api/axiosInstance.ts
+++ src/api/axiosInstance.ts
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export const instance = axios.create({
     withCredentials: true,
-     baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/'
-    //baseURL: 'https://neko-back.herokuapp.com/2.0/'
+     baseURL: process.env.REACT_APP_BACK_URL
 });
